385f578bb647db461f0a008df2bd6d5e2d78ec9e
Juan <114711949+juanpuhl7@users.noreply.github.com>
2022-10-10 20:10:38 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ejerc_cafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos a preparar un cafe....");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Caliente agua...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Ponga el saquito de cafe en la taza.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Ponga azucar a gusto");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Poner el agua caliente en la taza");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("revuelva....");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Disfrute del CAFE!!!");
+            Console.ReadKey();
+            Console.Clear();  
+        }
+    }
+}
